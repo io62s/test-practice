ff78228470d2076dbece2e649e46186d8bcbcdd3
io62s <ufo62s@gmail.com>
2019-11-08 19:44:28 +0100

    testing redux hooks, practice...

---
+++ hooks-redux-practice/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import useInput from "./useInput";
+import { useSelector, useDispatch } from "react-redux";
+function App() {
+  const [value, handleValueChange, resetValue] = useInput("");
+  const [email, handleEmailChange] = useInput("");
+  const count = useSelector(state => state.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>app</h1>
+      <input
+        type="text"
+        name="name"
+        value={value}
+        onChange={handleValueChange}
+      />
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <button onClick={resetValue} type="submit">
+        reset
+      </button>
+      <h2>{value}</h2>
+      <h2>{email}</h2>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
+      {count}
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+    </div>
+  );
+}
+
+export default App;
